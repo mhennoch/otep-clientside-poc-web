93d62249ace15ef452f648ce2b7dcdea8006f122
t2t2 <taavot@gmail.com>
2022-02-18 18:51:25 +0200

    Autodeploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.URL_BASE ?? '',
   build: {
     rollupOptions: {
       input: {
